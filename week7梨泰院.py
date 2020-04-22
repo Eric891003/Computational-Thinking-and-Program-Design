a38f2ef7fb131cc4755ee02c6fe182a857e5adad
Eric891003 <62084060+Eric891003@users.noreply.github.com>
2020-04-23 01:36:41 +0800

    Add files via upload

---
+++ week7梨泰院.py
@@ -0,0 +1,16 @@
+#名稱：梨泰院class
+#wiki網址：https://zh.wikipedia.org/wiki/梨泰院Class
+#放映日期：2020年1月31日
+#主演：朴敘俊、金多美、權娜拉、劉在明
+#導演：金成允
+
+movie_title ="이태원 클라쓰" #梨泰院class
+release_year="2020" #2020年1月31日
+star_1234="박서준、김다미、권나라、유재명"#朴敘俊、金多美、權娜拉、劉在明
+director="김성윤"#金成允
+
+print(movie_title)#螢幕列印
+print(release_year)#螢幕列印
+print(star_1234)#螢幕列印
+print(director)#螢幕列印
+
